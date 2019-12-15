b533ff41c97f86deec254dd55cb2858414572d4d
wenhq <null@null.com>
2019-12-15 21:10:29 +0800

    自动格式化

--- src/main/java/waditu/tushare/common/HTTParty.java
+++ src/main/java/waditu/tushare/common/HTTParty.java
@@ -12,24 +12,25 @@ import java.io.IOException;
  * Created by Raymond on 25/12/2016.
  */
 public class HTTParty {
-    public static String get(String url, String charEncode) {
-        if(charEncode == null || charEncode.trim().length() == 0) {
-            charEncode = "UTF-8";
-        }
-        HttpClient client = HttpClients.createDefault();
-        HttpGet request = new HttpGet(url);
-        String responseContent = null;
-        try {
-            HttpResponse response = client.execute(request);
-            if (response.getStatusLine().getStatusCode() == 200) {
-                responseContent = EntityUtils.toString(response.getEntity(), charEncode);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return responseContent;
-    }
-    public static String get(String url) {
-        return get(url, null);
-    }
+	public static String get(String url, String charEncode) {
+		if (charEncode == null || charEncode.trim().length() == 0) {
+			charEncode = "UTF-8";
+		}
+		HttpClient client = HttpClients.createDefault();
+		HttpGet request = new HttpGet(url);
+		String responseContent = null;
+		try {
+			HttpResponse response = client.execute(request);
+			if (response.getStatusLine().getStatusCode() == 200) {
+				responseContent = EntityUtils.toString(response.getEntity(), charEncode);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return responseContent;
+	}
+
+	public static String get(String url) {
+		return get(url, null);
+	}
 }
